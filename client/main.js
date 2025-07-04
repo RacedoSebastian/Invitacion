74d976c8cf2c166acbe5435ab403de0e1c3750a7
RacedoSebastian <racedosebas@gmail.com>
2025-07-04 17:10:08 -0300

    prueba de render

--- client/main.js
+++ client/main.js
@@ -54,7 +54,7 @@ form.addEventListener("submit", (e) => {
   console.log("🚀 ~ form.addEventListener ~ radio:", radio);
 
   // enviar los datos al back-end
-  fetch("http://localhost:5173/api/confirm", {
+  fetch("https://invitacion-cumpleanos.onrender.com/api/confirm", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
