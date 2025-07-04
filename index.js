93584a88af3d641752aebbd607c216fcaa770745
RacedoSebastian <racedosebas@gmail.com>
2025-07-04 20:34:43 -0300

    agregando origen de invitacion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const options = {
-  origin: [process.env.ORIGEN, "http://localhost:5173"],
+  origin: [process.env.ORIGEN, "https://invitacionjuanjo.vercel.app", "http://localhost:5173"],
   credentials: true,
 };
 
