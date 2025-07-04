ba0935f8a146125a37a33e9e303e12d3299723a2
RacedoSebastian <racedosebas@gmail.com>
2025-07-04 18:25:58 -0300

    error 500

--- client/main.js
+++ client/main.js
@@ -65,24 +65,12 @@ form.addEventListener("submit", (e) => {
     .then((data) => {
       console.log("��� ~ data:", data);
       if (!data.message) {
-        if (Array.isArray(data)) {
-          data.forEach((message) => {
-            console.log("🚀 ~ data.forEach ~ message:", message.issue);
-            errors.innerHTML = message.issue;
-            submitButton.disabled = false;
-          });
-        } else if (data.error) {
-          errors.innerHTML = data.error;
+        data.map((message) => {
+          console.log("🚀 ~ data.map ~ message:", message.issue);
+          errors.innerHTML = message.issue;
           submitButton.disabled = false;
-        }
+        });
         return;
-        // if (!data.message) {
-        //   data.map((message) => {
-        //     console.log("🚀 ~ data.map ~ message:", message.issue);
-        //     errors.innerHTML = message.issue;
-        //     submitButton.disabled = false;
-        //   });
-        //   return;
       }
       alert(data.message);
       errors.innerHTML = "";
