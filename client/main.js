9cdcfa6b516a25b82de114cad89137e0af70d907
RacedoSebastian <racedosebas@gmail.com>
2025-12-17 15:53:54 -0300

    asignacion de url de onrender

--- client/main.js
+++ client/main.js
@@ -44,7 +44,7 @@ form.addEventListener("submit", (e) => {
   console.log("🚀 ~ form.addEventListener ~ radio:", radio);
 
   // enviar los datos al back-end
-  fetch("http://localhost:3000/api/confirm", {
+  fetch("https://invitacion-morena.onrender.com/api/confirm", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
