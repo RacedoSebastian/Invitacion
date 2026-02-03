2448aa9d42bcc8e9f957b78620c48de16c2196b4
RacedoSebastian <racedosebas@gmail.com>
2026-02-03 01:36:24 -0300

    cambios de estrutura

--- client/main.js
+++ client/main.js
@@ -3,9 +3,9 @@ import "./css/style.css";
 //main
 simplyCountdown("#app", {
   year: 2026, // required
-  month: 1, // required
-  day: 17, // required
-  hours: 22, // Default is 0 [0-23] integer
+  month: 2, // required
+  day: 8, // required
+  hours: 16, // Default is 0 [0-23] integer
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: {
--- frontend/main.js
+++ frontend/main.js
@@ -24,10 +24,10 @@ document.querySelector("#app").innerHTML = `
 
 setupCounter(document.querySelector("#counter"));
 simplyCountdown(".app", {
-  year: 2024, // required
-  month: 9, // required
-  day: 25, // required
-  hours: 0, // Default is 0 [0-23] integer
+  year: 2026, // required
+  month: 2, // required
+  day: 8, // required
+  hours: 16, // Default is 0 [0-23] integer
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: {
