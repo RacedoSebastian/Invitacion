ccdf5ce59648fe0b21e5e9e2b95a11777f4f7bc5
RacedoSebastian <racedosebas@gmail.com>
2026-03-26 00:26:58 +0800

    cambio de estructura

--- client/main.js
+++ client/main.js
@@ -2,10 +2,10 @@ import "./css/style.css";
 
 //main
 simplyCountdown("#app", {
-  year: 2024, // required
-  month: 8, // required
+  year: 2026, // required
+  month: 4, // required
   day: 24, // required
-  hours: 22, // Default is 0 [0-23] integer
+  hours: 13, // Default is 0 [0-23] integer
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: {
